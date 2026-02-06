6c02d8bacaccc6dff995ddd7eb7ed4027b69aaef
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-02-05 21:59:52 -0800

    New updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter as Router} from 'react-router'
+import { BrowserRouter as Router } from 'react-router'
+import 'boxicons'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+import {Menu} from '@boxicons/react';
 import { NavLink } from "react-router";
 
 export default function SideBar() {
+  const [open, setOpen] = useState(false)
   
   return (
     <div className="sidebar">
       <h2>Blog Post app</h2>
       <nav>
-        <ul>
-          <li>
-            <NavLink to="/" style={{textDecoration: 'none'}}>Home</NavLink></li>
+        <Menu onClick={() => setOpen(!open)} className="menu"/>
+        <ul className={`nav-links ${open ? "show" : ""}`}>
+          <li className='nav'>
+            <NavLink to="/" style={{ textDecoration: 'none' }}
+             onClick={() => {setOpen(false)}}>Home</NavLink>
+          </li>
         </ul>
       </nav>
     </div>
   )
 }
+
